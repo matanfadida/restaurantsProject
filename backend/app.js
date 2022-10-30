05acf8af1924bd7464f4d066843b003a17fdab41
matanfadida <73058061+matanfadida@users.noreply.github.com>
2022-10-30 20:35:56 +0200

    backend open

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(3000);
