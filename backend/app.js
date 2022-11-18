5df98810ebfafb6d345b52f1842acb0950268bd1
matanfadida <73058061+matanfadida@users.noreply.github.com>
2022-11-18 15:40:49 +0200

    start router

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const adminRouts = require('./routes/admin');
 const shopRouts = require('./routes/shop');
+const shefRouts = require('./routes/chef');
 const mongodbConnect = require('./util/database').mongodbConnect;
 
 const express = require('express');
@@ -10,9 +11,9 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res) => {});
-// app.use('/admin',adminRouts);
-// app.use(shopRouts);
+app.use('/admin',adminRouts);
+app.use('/shef', shefRouts);
+app.use(shopRouts);
 
 mongodbConnect((client) => {
     console.log(client);
