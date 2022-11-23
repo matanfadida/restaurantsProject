dd40b5aea9d97a059efb05afa87a4d5ac72be57d
matanfadida <73058061+matanfadida@users.noreply.github.com>
2022-11-23 21:40:21 +0200

    install package

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -2,6 +2,33 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+
+const order = [
+    {
+      id: 1,
+      name: "הלחם שלנו",
+      detail: "לחם פראנה באפייה מסורתית,שמן זית ומטבלים טעימים",
+      price: 26,
+    },
+    {
+      id: 2,
+      name: "טחינה ספייס",
+      detail:
+        "טחינה לבנה, בשר עגל קצוץ מתובל בתערובת תבלינים מסורתית,  צנוברים ועשבי תיבול",
+      price: 46,
+    },
+    {
+      id: 3,
+      name: "טחינה ספייס",
+      detail:
+        "טחינה לבנה, בשר עגל קצוץ מתובל בתערובת תבלינים מסורתית,  צנוברים ועשבי תיבול",
+      price: 46,
+    },
+  ];
+
+router.use("/", (req, res, next) => {
+    console.log('here');
+    res.json(order);
+});
 
 module.exports = router;
