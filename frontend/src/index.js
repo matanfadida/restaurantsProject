f4396c7ec1204350b2e1397bd286c59cb1ba27b0
matanfadida <73058061+matanfadida@users.noreply.github.com>
2022-11-27 19:39:03 +0200

    router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import CartProvider from "./state/cart-provider";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <App/>
-    </CartProvider>
+    <Router>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </Router>
   </React.StrictMode>
 );
