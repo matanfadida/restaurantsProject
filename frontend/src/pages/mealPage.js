2b40f9c13f33e956b8605e354e688f90f7c0d0e8
haimrubin <73288124+haimrubin@users.noreply.github.com>
2023-04-18 22:36:50 +0300

    .

--- frontend/src/pages/mealPage.js
+++ frontend/src/pages/mealPage.js
@@ -1,13 +1,53 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import classes from "./mealPage.module.css";
 import Stars from "../components/Rating/ratingStar";
 import Comment from "./comment";
+
+import CartContext from "../state/buy-context";
+import { ImPlus, ImMinus } from "react-icons/im";
+import { BiRestaurant } from "react-icons/bi";
+import { v4 as uuidv4 } from "uuid";
 
 const Meal = () => {
   const params = useParams();
   const [loading, setLoading] = useState(true);
   const [product, setProduct] = useState();
+  const ctx = useContext(CartContext);
+  const [remark, setRemark] = useState("");
+  const [amount, setAmount] = useState(1);
+
+  const buttonAddItemHanlder = () => {
+    ctx.AddItem({
+      id: params.productId,
+      guid_id: uuidv4(),
+      name: product.product.name,
+      detail: product.product.detail,
+      price: product.product.price,
+      remark: remark,
+      amount: +amount,
+      status: "נשלח לטבח",
+    });
+    setRemark("");
+    setAmount(1);
+  };
+
+  const remarkChangeHandler = (event) => {
+    setRemark(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const plusHanlder = () => {
+    const temp = amount + 1;
+    setAmount(temp);
+  };
+
+  const minusHanlder = () => {
+    const temp = amount - 1;
+    setAmount(temp);
+  };
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -56,6 +96,40 @@ const Meal = () => {
         {product.comments.map((comment) => (
           <Comment key={comment._id} comment={comment.comment} />
         ))}
+      </div>
+
+      <div>
+        <input
+          className={classes.remark}
+          type="text"
+          onChange={remarkChangeHandler}
+          value={remark}
+          placeholder="הערות לטבח"
+        ></input>
+      </div>
+      <div className={classes.div}>
+        <div>
+          <button
+            className={classes.plus_button}
+            onClick={buttonAddItemHanlder}
+          >
+            הוסף
+            <BiRestaurant />
+          </button>
+          <button className={classes.plus_button} onClick={plusHanlder}>
+            <ImPlus />
+          </button>
+          <input
+            className={classes.amount}
+            value={amount}
+            type="number"
+            min={0}
+            onChange={amountChangeHandler}
+          ></input>
+          <button className={classes.plus_button} onClick={minusHanlder}>
+            <ImMinus />
+          </button>
+        </div>
       </div>
     </div>
   );
