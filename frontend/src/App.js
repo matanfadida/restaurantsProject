583c5126495f0a996f9c6371b6eb9bda07e14957
haimrubin <73288124+haimrubin@users.noreply.github.com>
2023-05-03 18:10:12 +0300

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useContext} from "react";
+import { Fragment, useContext } from "react";
 import Home from "./components/Home";
 import Error404 from "./components/error404";
 import Cart from "./components/NavigationBar/Cart/ShowCart";
@@ -10,7 +10,7 @@ import AddProduct from "./components/Admin/Poduct/add-product";
 import Tables from "./components/Admin/Tables";
 import TableDetail from "./components/Admin/TableDetail";
 import Header from "./components/NavigationBar/MainHeader/Header";
-import Login from "./components/Admin/Login/admin-login"; 
+import Login from "./components/Admin/Login/admin-login";
 import Meal from "./pages/mealPage";
 import Rate from "./components/Rating/rate";
 import AddCategory from "./components/Admin/Poduct/add-category";
@@ -18,36 +18,101 @@ import Loader from "./components/UI/loader";
 import Contact from "./pages/contact";
 import EditContact from "./components/Admin/EditContact";
 
-
 const App = () => {
   const ctx = useContext(CartContext);
 
-  if(ctx.isLoading){
-    return <Loader/>
+  if (ctx.isLoading) {
+    return <Loader />;
   }
 
   return (
     <Fragment>
-      <Header/>
+      <Header />
       {ctx.cartShow && <Cart />}
-      <Routes>
-        <Route path="/chef" element={<Chef />} />
-        <Route path="/product/:productId" element={<Meal/>} />
-        <Route path="/rate/:productId" element={<Rate />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin/add-product" element={ctx.isLogged ? <AddProduct /> : <Navigate replace to="/admin/login" />} />
-        <Route path="/admin/contact" element={ctx.isLogged ? <EditContact /> : <Navigate replace to="/admin/login" />} />
-        <Route path="/admin/edit-product/:productId" element={ctx.isLogged ? <AddProduct /> : <Navigate replace to="/admin/login" />} />
-        <Route path="/admin/tables" element={ctx.isLogged ? <Tables /> : <Navigate replace to="/admin/login" />} />
-        <Route path="/admin/tables/:tableId" element={ctx.isLogged ? <TableDetail /> : <Navigate replace to="/admin/login" />} />
-        <Route path="/admin/login" element={ctx.isLogged ? <Navigate replace to="/admin" /> : <Login />} />
-        <Route path="/admin/add-category" element={ctx.isLogged ? <AddCategory/> : <Navigate replace to="/admin/login" />} />
-        <Route path="/admin/signup" element={<Login />} />
-        <Route path="/admin" element={ctx.isLogged ? <AdminHome /> : <Navigate replace to="/admin/login" />} />
-        <Route path="/:restaurantName/:tableId" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
+      <div style={{marginTop:"70px"}}>
+        <Routes>
+          <Route path="/chef" element={<Chef />} />
+          <Route path="/product/:productId" element={<Meal />} />
+          <Route path="/rate/:productId" element={<Rate />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/admin/add-product"
+            element={
+              ctx.isLogged ? (
+                <AddProduct />
+              ) : (
+                <Navigate replace to="/admin/login" />
+              )
+            }
+          />
+          <Route
+            path="/admin/contact"
+            element={
+              ctx.isLogged ? (
+                <EditContact />
+              ) : (
+                <Navigate replace to="/admin/login" />
+              )
+            }
+          />
+          <Route
+            path="/admin/edit-product/:productId"
+            element={
+              ctx.isLogged ? (
+                <AddProduct />
+              ) : (
+                <Navigate replace to="/admin/login" />
+              )
+            }
+          />
+          <Route
+            path="/admin/tables"
+            element={
+              ctx.isLogged ? <Tables /> : <Navigate replace to="/admin/login" />
+            }
+          />
+          <Route
+            path="/admin/tables/:tableId"
+            element={
+              ctx.isLogged ? (
+                <TableDetail />
+              ) : (
+                <Navigate replace to="/admin/login" />
+              )
+            }
+          />
+          <Route
+            path="/admin/login"
+            element={
+              ctx.isLogged ? <Navigate replace to="/admin" /> : <Login />
+            }
+          />
+          <Route
+            path="/admin/add-category"
+            element={
+              ctx.isLogged ? (
+                <AddCategory />
+              ) : (
+                <Navigate replace to="/admin/login" />
+              )
+            }
+          />
+          <Route path="/admin/signup" element={<Login />} />
+          <Route
+            path="/admin"
+            element={
+              ctx.isLogged ? (
+                <AdminHome />
+              ) : (
+                <Navigate replace to="/admin/login" />
+              )
+            }
+          />
+          <Route path="/:restaurantName/:tableId" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </div>
     </Fragment>
   );
 };
--- frontend/src/components/Admin/EmailForm.js
+++ frontend/src/components/Admin/EmailForm.js
@@ -162,7 +162,7 @@ const EmailForm = () => {
           )}
           <textarea
             name="message"
-            placeholder=" שתף אותנו בחווית המסעדה שלך "
+            placeholder="שתפו אותנו בחווית המסעדה שלכם "
             value={msgValue}
             onChange={msgChangeHandler}
             onBlur={msgBlurHandler}
