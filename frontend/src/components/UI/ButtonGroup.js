44b0eef316d0fe04e783da04a895f6f31c39f829
haimrubin <73288124+haimrubin@users.noreply.github.com>
2023-05-05 11:35:42 +0300

    button group ui (chef use)

--- frontend/src/components/UI/ButtonGroup.js
+++ frontend/src/components/UI/ButtonGroup.js
@@ -1,12 +1,30 @@
-import classes from './Button.module.css'
+import classes from "./Button.module.css";
+import { useState } from "react";
 
-const ButtonGroup = () => {
+const ButtonGroup = (props) => {
   const [focus, setFocus] = useState();
+
+  const buttonClassName = (id) => {
+    const className = focus === id ? "clicked" : "notClicked";
+    return classes[className];
+  };
+  const clickHandler = (event, id, onClick) => {
+    setFocus(id);
+    onClick();
+  };
+
+  const buttons = props.buttons.map((item) => (
+    <button
+      className={buttonClassName(item.id)}
+      onClick={(event) => clickHandler(event, item.id, item.onClick)}
+    >
+      {item.name}
+    </button>
+  ));
 
   return (
     <div className={classes.button}>
-      <button>בהכנה</button>
-      <button>מוכן</button>
+      {buttons}
     </div>
   );
 };
--- frontend/src/components/cuisine/chefProduct.js
+++ frontend/src/components/cuisine/chefProduct.js
@@ -1,24 +1,25 @@
-
-import Button from "../UI/Button";
+import ButtonGroup from "../UI/ButtonGroup";
 import classes from "./chefItem.module.css";
 
 const chefProduct = (props) => {
-  const InProgresHandler = async () => {const response = await fetch(`/api/chef/edit-status`, {
-    method: "POST",
-    body: JSON.stringify({
-      idOrder: props.orderId,
-      idProduct: props.product.guid_id,
-      numberTable: props.numberTable,
-      status: "בהכנה",
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
-  if (!response.ok) {
-    throw new Error("Request failed!");
-  }
-  const result = await response.json();
+  const InProgresHandler = async () => {
+    const response = await fetch(`/api/chef/edit-status`, {
+      method: "POST",
+      body: JSON.stringify({
+        idOrder: props.orderId,
+        idProduct: props.product.guid_id,
+        numberTable: props.numberTable,
+        status: "בהכנה",
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (!response.ok) {
+      throw new Error("Request failed!");
+    }
+    const result = await response.json();
 
-  console.log(result);};
+    console.log(result);
+  };
 
   const ReadyHandler = async () => {
     const response = await fetch(`/api/chef/edit-status`, {
@@ -41,32 +42,17 @@ const chefProduct = (props) => {
   //   className={classes.buttons}
   return (
     <li className={classes.chefProduct}>
-      <div>
-        <Button onClick={ReadyHandler}>מוכן</Button>
-        <Button onClick={InProgresHandler}>בהכנה</Button>
-      </div>
+     
+      <ButtonGroup
+        buttons={[
+          { id: 1, onClick: ReadyHandler, name: "מוכן" },
+          { id: 2, onClick: InProgresHandler, name: "בהכנה" },
+        ]}
+      ></ButtonGroup>
       <span>{props.product.name}</span>
     </li>
   );
 };
 
-{
-  /* <li>
-      <div>
-        <span>{props.product.name}</span>
-        <div>
-          <ul >
-            <li>
-              <Button onClick={InProgresHandler}>בהכנה</Button>
-            </li>{" "}
-            <li>
-              {" "}
-              <button onClick={ReadyHandler}>מוכן</button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </li> */
-}
 
 export default chefProduct;
