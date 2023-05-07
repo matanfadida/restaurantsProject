4ac4427df460a639802e3686cb85663c9271e287
haimrubin <73288124+haimrubin@users.noreply.github.com>
2023-05-07 22:00:07 +0300

    chef/bar using the ui buttongroup

--- frontend/src/components/cuisine/chef.js
+++ frontend/src/components/cuisine/chef.js
@@ -2,18 +2,16 @@ import ChefItem from "./chefItem";
 import classes from "./chef.module.css";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
+import ButtonGroup from "../UI/ButtonGroup";
 
 const Chef = (props) => {
   const [orders, setOrders] = useState([]);
   const [isChef, setIsChef] = useState(true);
 
-
-  
-
   const isChefHandler = () => {
-    const temp = !isChef
-    setIsChef(temp)
-  }
+    const temp = !isChef;
+    setIsChef(temp);
+  };
 
   useEffect(() => {
     const socket = io("http://localhost:5000/"); // Replace with your server URL
@@ -45,7 +43,6 @@ const Chef = (props) => {
     />
   ));
 
-
   const barList = orders.map((order) => (
     <ChefItem
       orderId={order._id}
@@ -55,15 +52,17 @@ const Chef = (props) => {
     />
   ));
 
-
-
-
-
   return (
     <div className={classes.chef}>
+      
       <div className={classes.buttons}>
-        <button onClick={isChefHandler} className={isChef ? classes.off : classes.on}>בר</button>
-        <button onClick={isChefHandler} className={isChef ? classes.on : classes.off}>מטבח</button>
+        <ButtonGroup
+          focus={2}
+          buttons={[
+            { id: 1, onClick: isChefHandler, name: "בר" },
+            { id: 2, onClick: isChefHandler, name: "מטבח" },
+          ]}
+        ></ButtonGroup>
       </div>
 
       {isChef && <ul>{chefList}</ul>}
